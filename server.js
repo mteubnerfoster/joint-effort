737e2861ea5b6fb0015aad65bb8a2ed72930e630
Cosdaman <Cosdaguy@gmail.com>
2021-12-21 19:42:53 -0800

    added preliminary server connections

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-const routes = require('./controllers');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
