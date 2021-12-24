4006137b6034572305dedfa4cceda8ced7072b97
Cosdaman <Cosdaguy@gmail.com>
2021-12-23 21:41:31 -0800

    added personal testing routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes')
+const testRoutes = require('./testingRoutes')
 
 router.use('/', homeRoutes);
+router.use('/test', testRoutes);
+
 
 module.exports = router;
 
