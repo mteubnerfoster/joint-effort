7df3bfaab82714dc71bfff78ee2d364ac4a0999d
Oscar Plumridge <Bigop76.op@gmail.com>
2021-12-31 11:28:05 -0800

    authentication changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,5 @@
 const router = require('express').Router()
+const userRoutes = require('./userRoutes')
+
+router.use('/users', userRoutes)
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const homeRoutes = require('./homeRoutes')
-
+const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
 
