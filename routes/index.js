e08fcc4300a5f7c5e16a090167160e1b1aa9efac
Cosdaman <Cosdaguy@gmail.com>
2021-12-31 12:07:08 -0800

    merge

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,8 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const homeRoutes = require('./homeRoutes')
-const testRoutes = require('./testingRoutes')
-
-router.use('/', homeRoutes);
-router.use('/test', testRoutes);
-
-=======
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
->>>>>>> 08ca5fcef99b64ebe46ebfe3761720caa7461517
 
 module.exports = router;
 
