53540e2b32457e4da967d24f935aff6554766c61
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2022-01-04 00:26:07 -0800

    updated secret in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Super secret boogaloo',
+    secret: 'process.env.COOKIE_SECRET',
     cookie: {},
     resave: false,
     saveUninitialized: true,
