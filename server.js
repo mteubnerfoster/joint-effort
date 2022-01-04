ba808f6d2cb48454fa805dc8d8370fd878dfbff2
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2022-01-03 23:42:16 -0800

    removed const routes to test. will add back if needed

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
