7e83cf2dbca53b8d514de5eae67a71b3675d098c
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2022-01-04 00:54:57 -0800

    added async info for updating user data

--- models/User.js
+++ models/User.js
@@ -39,10 +39,15 @@ User.init(
     },
     {
         hooks: {
-            beforeCreate: async (newUserData) => {
+            async beforeCreate(newUserData) {
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
         },
         sequelize,
         timestamps: false,
