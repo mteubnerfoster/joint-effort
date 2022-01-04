b0c9c9881a6929e54253524b7742e227ec047aed
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2022-01-03 23:38:50 -0800

    updated routes name to controllers, added app.use(require('./controllers/')); to server.js file

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const homeRoutes = require('./homeRoutes');
+
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 const foodRoutes = require('./foodRoutes')
 
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 router.use('/food', foodRoutes);
 
 module.exports = router;
