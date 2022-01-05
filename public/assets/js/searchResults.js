3b0f1dfb58f30f42de72066f4f169b710d49511b
Cosdaman <Cosdaguy@gmail.com>
2022-01-04 21:00:58 -0800

    updated search results

--- public/assets/js/searchResults.js
+++ public/assets/js/searchResults.js
@@ -1,23 +1,27 @@
 console.log('food results')
 
-let foodLat;
-let foodLong;
-let foodName;
+let foodLat, dispLat;
+let foodLong, dispLong;
+let foodName, dispName;
+let foodNameEl;
 
 $('.yelpFood').click(foodtruckClick)
+$('.yelpPlant').click(dispensaryClick)
 
 async function foodtruckClick() {
+    foodNameEl = await $(this).find('.foodName')
     foodLat = await $(this).data("lat");
     foodLong = await $(this).data("long");
-    foodName = await $(this).data("alias");
-    foodId = await $(this).data("id");
-    let storeInfo = {
+    foodName = await foodNameEl.text();
+    let storeFoodInfo = {
         foodLat,
         foodLong,
-        foodName,
-        foodId
+        foodName
     }
-    localStorage.setItem('chosenStoreInfo', JSON.stringify(storeInfo));
-    console.log($(this).find($('foodName')))
+    localStorage.setItem('chosenFoodStoreInfo', JSON.stringify(storeFoodInfo));
     document.location.replace(`/plant`);
+}
+
+async function dispensaryClick() {
+    console.log('dispensary click')
 }
